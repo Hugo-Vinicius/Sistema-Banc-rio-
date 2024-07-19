dac8458f19066e692a2c736591f413016561eb91
Hugo-Vinícius <132942472+Hugo-Vinicius@users.noreply.github.com>
2024-07-19 18:26:00 -0300

    Add files via upload

---
+++ BancoPoo.java/SistemaBancário.java/src/ContaCorrente.java
@@ -0,0 +1,10 @@
+public class ContaCorrente extends Conta {
+
+    public void imprimirExtrato() {
+        System.out.println("Extrato Conta");
+        System.out.println(String.format("Agência: %d", super.agencia));
+        System.out.println(String.format("Agência: %d", super.agencia));
+        System.out.println(String.format("Agência: %d", super.agencia));
+    }
+  
+}
